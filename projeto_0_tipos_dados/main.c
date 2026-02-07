9a30a036320633efb8dcc5a6850d2298f541f0bc
dfponte <danielpontep@hotmail.com>
2026-02-07 17:50:21 -0300

    Proj. Log. Prog.

---
+++ projeto_0_tipos_dados/main.c
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+
+
+int main(){
+
+   char c1 = 127;
+
+   printf("Valor maximo tipo char %d",c1);
+
+    return 0;
+}
---
+++ projeto_1_tipos_dados/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+
+int main(){
+
+   short int c1 = 32767;
+
+   printf("Valor maximo tipo int %d",c1);
+
+    return 0;
+}
