08c8e7890df578f63d12b5383fe7c5553bce03b7
dfponte <danielpontep@hotmail.com>
2026-02-07 19:07:22 -0300

    commit_07_02_26

--- build/CMakeFiles/my_math.dir/src/math.c.obj.d
+++
@@ -1,8 +0,0 @@
-CMakeFiles/my_math.dir/src/math.c.obj: \
- C:\Users\danie\Cursos\Logica_Programacao\Projetos_C_Log_Prog\projeto_base_cmake\src\math.c \
- c:\mingw\include\stdio.h c:\mingw\include\_mingw.h \
- c:\mingw\include\msvcrtver.h c:\mingw\include\w32api.h \
- c:\mingw\include\sdkddkver.h \
- c:\mingw\lib\gcc\mingw32\6.3.0\include\stddef.h \
- c:\mingw\include\sys/types.h \
- c:\mingw\lib\gcc\mingw32\6.3.0\include\stdarg.h
--- projeto_base_cmake/src/infox.c
+++ projeto_base_cmake/src/infox.c
@@ -1,3 +1,5 @@
+
+#include <stdio.h>
 void inf1(void){
 
 printf("Minha inf1!!\n");
